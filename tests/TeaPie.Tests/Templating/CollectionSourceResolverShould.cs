1c80174313b590181aba517099e9ebf96363d258
Kantorová Michaela <kantorova@kros.sk>
2026-08-17 12:03:49 +0200

    test: cover numeric range, unknown variable, and non-enumerable resolver cases

--- tests/TeaPie.Tests/Templating/CollectionSourceResolverShould.cs
+++ tests/TeaPie.Tests/Templating/CollectionSourceResolverShould.cs
@@ -17,4 +17,37 @@ public class CollectionSourceResolverShould
         source.ItemCount.Should().Be(3);
         source.Collection.Should().NotBeNull();
     }
+
+    [Fact]
+    public void ResolveNumericRangeWithoutAnyCollectionVariable()
+    {
+        var resolver = new CollectionSourceResolver(new global::TeaPie.Variables.Variables());
+
+        var source = resolver.Resolve("(1..5)");
+
+        source.ItemCount.Should().Be(5);
+        source.Collection.Should().BeNull();
+    }
+
+    [Fact]
+    public void ThrowWhenCollectionVariableIsNotFound()
+    {
+        var resolver = new CollectionSourceResolver(new global::TeaPie.Variables.Variables());
+
+        var act = () => resolver.Resolve("DoesNotExist");
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("*DoesNotExist*");
+    }
+
+    [Fact]
+    public void ThrowWhenVariableIsNotEnumerable()
+    {
+        var variables = new global::TeaPie.Variables.Variables();
+        variables.SetVariable("NotACollection", 42);
+        var resolver = new CollectionSourceResolver(variables);
+
+        var act = () => resolver.Resolve("NotACollection");
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("*collection*");
+    }
 }
